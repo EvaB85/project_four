a5b96db60bda282277de576c68119e747d64b618
Eva Barrett <Evabarrett23@Gmail.com>
2018-08-08 14:50:52 -0700

    added link tags

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,60 +1,28 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Playlist from './Playlist';
-import { withStyles } from '@material-ui/core/styles';
+import React, {Component} from 'react';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
-import './App.css';
-import {Link} from 'react-router-dom';
-import homescreenshot from './homescreenshot.png';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
 
-const styles = theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing.unit * 2,
-    margin: theme.spacing.unit * 2,
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  button: {
-    margin: theme.spacing.unit * 1
-  }
-});
-
-const Home = props => {
-  const { classes } = props;
-  let tryButton = (
-    <Link to='/loginsignup'>
-      <button className='home-button try-button' >Get started or login</button>
-    </Link>
-  );
-  if (props.user) {
-    tryButton = '';
-  }
-
-  return (
-    <div id="home-page" className={classes.root}>
-      <Grid container spacing={12}>
-        <Grid item xs={12}>
-        </Grid>
-        <Grid item xs={12} md={12}>
-          <Paper id="description" className={classes.paper}>
-
-            <p>Moodsic is a color analysis appication that generates a personalized
-              playlist. Simply upload a photo, select a genre and create your playlist!</p>
-
-          </Paper>
-          {tryButton}
-        </Grid>
-      </Grid>
-      <div>
-        <img id='sample-photo' src={homescreenshot}></img>
+class Home extends Component {
+  render() {
+    return (
+      <div className="Home">
+        <div>
+          <Paper className="paper">
+    				<h3>ONE Home</h3>
+    			</Paper>
+        </div>
+        <div className="root">
+          <Grid container >
+            <Grid item xs={12}>
+              <p className="grid" >GRID in Home</p>
+            </Grid>
+          </Grid>
+          <Grid container spacing={24} className="name">
+          </Grid>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
 }
 
-export default withStyles(styles)(Home);
+export default Home;
