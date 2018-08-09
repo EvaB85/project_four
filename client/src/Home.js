73ae1e503fc3d992be190fa5bb4c06bf568c7dd3
Eva Barrett <Evabarrett23@Gmail.com>
2018-08-09 12:39:41 -0700

    fixing divs

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,7 +10,28 @@ class Home extends Component {
           <Paper className="paper">
     				<h3>ONE Home</h3>
 
-            
+            <div class="row">
+              <div class="col s12 m4 offset-m4">
+                <div class="card">
+
+                  <div class="card-action team lighten-1 white-text">
+                    <h3>Login Form</h3>
+                  </div>
+                  <div class="card-content">
+                    <div class="form-field">
+                      <label for ="username">Username</label>
+                      <input type="text" id="username">
+                      </div><br>
+                  <div class="form-field">
+                    <button class="btn-large waves-effect waves-dark" style="width=100%;">Login</button>
+                  </div><br>
+                    </div>
+
+                </div>
+              </div>
+            </div>
+
+
 
     			</Paper>
         </div>
