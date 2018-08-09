e81200731c56bba7bd1d9631507eeecd6b6a3ab4
Eva Barrett <Evabarrett23@Gmail.com>
2018-08-09 14:12:23 -0700

    login form not working

--- client/src/Home.js
+++ client/src/Home.js
@@ -6,34 +6,48 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
+
+        <div class="row">
+          <div class="col s12 m4 offset-m4">
+            <div class="card">
+
+              <div class="card-action teal lighten-1 white-text">
+                <h3>Login form </h3>
+              </div>
+
+              <div class="card-content">
+                <div class="form-field">
+                  <label form="username">Username</label>
+                  <input type="text" id="username"></input>
+                </div>
+
+                <div class="form-field">
+                  <label form="password">Password</label>
+                  <input type="password" id="password"></input>
+                </div>
+
+                <div class="form-field">
+                  <input type="checkbox" id="remem"></input>
+                  <label form="remem">Remember Me</label>
+                </div>
+
+                <div class="form-field">
+                  <button class="btn-large waves-effect waves-dark" style="width=100%">Login</button>
+
+
+                </div>
+
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
         <div>
           <Paper className="paper">
     				<h3>ONE Home</h3>
-
-            <div class="row">
-              <div class="col s12 m4 offset-m4">
-                <div class="card">
-
-                  <div class="card-action team lighten-1 white-text">
-                    <h3>Login Form</h3>
-                  </div>
-                  <div class="card-content">
-                    <div class="form-field">
-                      <label for ="username">Username</label>
-                      <input type="text" id="username">
-                      </div><br>
-                  <div class="form-field">
-                    <button class="btn-large waves-effect waves-dark" style="width=100%;">Login</button>
-                  </div><br>
-                    </div>
-
-                </div>
-              </div>
-            </div>
-
-
-
-    			</Paper>
+          </Paper>
         </div>
         <div className="root">
           <Grid container >
